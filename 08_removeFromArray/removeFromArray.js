6dde4c0a8c884efc9cc45045d2264c31c272b197
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-10 21:33:31 +0900

    Create a function that takes an array and returns a modified array with things you want to remove

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(receivedArray, ...removingElements) {
+    return receivedArray.filter(element => !removingElements.includes(element));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
