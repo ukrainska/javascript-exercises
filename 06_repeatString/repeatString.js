07840a172020683ad477833c502d7a4332af5ffd
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-10 20:40:59 +0900

    Create a fuction that takes string argument and return number of times given

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let result = "";
+    if (number < 0) return "ERROR";
+    for (let i = 0; i < number; i++){
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
