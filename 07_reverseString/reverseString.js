71a44140315614a22d663c0c342005298a4c3195
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-10 20:51:23 +0900

    Create a function where a word being returned in reverse

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {//"hello"
+    let reversedWord = "";
+    for (let i = word.length-1; i >= 0; i--)
+    {
+        reversedWord += word[i];
+    }
+    console.log("reversedWord");
+    return reversedWord;
 };
 
 // Do not edit below this line
