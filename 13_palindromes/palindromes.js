b99d8da970f61455a9b0ed7ad96cf2b34a6b9726
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-20 10:01:04 +0900

    Finish palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let filteredSentence = sentence
+    .toLowerCase()
+    .split("")
+    .filter(character => alphanumerical.includes(character))
+    .join("");
 
+    let newSentence = filteredSentence.split("").reverse().join("");
+    return newSentence === filteredSentence;
 };
 
 // Do not edit below this line
